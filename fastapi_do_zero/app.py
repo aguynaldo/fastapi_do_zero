a9c9a461e343a45b3e5b142105d84f05a779236c
Aguinaldo S Freitas <aguynaldo@gmail.com>
2024-03-14 16:17:18 -0300

    Iniciando a validação com pydantic e criando o post para C (CRUD).

--- fastapi_do_zero/app.py
+++ fastapi_do_zero/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from fastapi_do_zero.schemas import Message, UserPublic, UserSchema
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo'}
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user
