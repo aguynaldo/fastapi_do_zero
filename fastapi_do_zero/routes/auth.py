da2cd3529387e3429bd4acb3e166c629903416df
Aguinaldo S Freitas <aguynaldo@gmail.com>
2024-04-03 23:15:02 -0300

    Refatorando os códigos em arquivos específicos. Antes de iniciar a parte das routas com Annoted.

---
+++ fastapi_do_zero/routes/auth.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from fastapi_do_zero.database import get_session
+from fastapi_do_zero.models import User
+from fastapi_do_zero.schemas import Token
+from fastapi_do_zero.security import create_access_token, verify_password
+
+router = APIRouter(prefix='/auth', tags=['auth'])
+
+
+@router.post('/token', response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user:
+        raise HTTPException(status_code=400, detail='Usuário não encontrado.')
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=400, detail='Usuário ou senha inválido.'
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
---
+++ tests/test_users.py
@@ -0,0 +1,122 @@
+from fastapi_do_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret123',
+        },
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_create_user_already_exist(client, user):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'Teste',
+            'email': 'batatinha@frita.com',
+            'password': '123mudeiagora',
+        },
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username já cadastrado.'}
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_read_user_specific(client, user):
+    response = client.get('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'Teste',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_read_user_specific_not_found(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Usuário não encontrado.'}
+
+
+def test_udpate_user(client, user, token):
+    response = client.put(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@sponja.com',
+            'password': 'mudeinoteste',
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@sponja.com',
+        'id': 1,
+    }
+
+
+def test_update_user_not_exist(client, user, token):
+    response = client.put(
+        '/users/2',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@sponja.com',
+            'password': 'mudeinoteste',
+        },
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        'detail': 'Não tem permissão para executar essa função.'
+    }
+
+
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Usuário deletado com sucesso.'}
+
+
+def test_delete_user_not_exist(client, token):
+    response = client.delete(
+        '/users/2',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {
+        'detail': 'Não tem permissão para executar essa função.'
+    }
